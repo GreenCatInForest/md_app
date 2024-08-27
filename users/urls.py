e79ab0102b21cdb3c4b3f7eb8defaa869b5325df
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-08-27 11:27:14 +0100

    gen reports fix the style main

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import password_reset_request_view, password_reset_confirm_view
+
 from . import views
 
 urlpatterns = [
@@ -8,8 +8,7 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('logout_page/', views.user_logout, name='logout_page'),
-    path('password-reset/', password_reset_request_view, name='password_reset_request'),
-    path('reset/<uidb64>/<token>/', password_reset_confirm_view, name='password_reset_confirm'),
+ 
     
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
