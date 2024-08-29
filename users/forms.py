33824ccd8649ced2edb45e8d3123b18c1b093340
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-08-29 20:36:14 +0100

    upd design

--- users/forms.py
+++ users/forms.py
@@ -18,7 +18,7 @@ class UserLoginForm(forms.Form):
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={
-                'class': 'w-full h-12 py-3 mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm font-semibold rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
+                'class': 'input-field',
                 'placeholder': 'Enter your email',
                 'id': 'email',
                 'required': True,
@@ -28,7 +28,7 @@ class UserLoginForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                'class': 'w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm font-semibold rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
+                'class': 'input-field',
                 'placeholder': 'Enter your password',
                 'id': 'password',
                 'required': True,
