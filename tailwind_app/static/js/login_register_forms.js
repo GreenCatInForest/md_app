6315c636d57e8aa096286fc2296e4ae039b64a17
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-08-30 02:08:54 +0100

    styled login page

--- tailwind_app/static/js/login_register_forms.js
+++ tailwind_app/static/js/login_register_forms.js
@@ -6,8 +6,8 @@ const registerForm = document.querySelector('.register-form');
 
 
 btnLoginForm.addEventListener('click', () => {
-    btnLoginForm.style.backgroundColor = '#21264D';
-    btnRegisterForm.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+    btnLoginForm.style.backgroundColor = 'rgb(29 78 216)';
+    btnRegisterForm.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
 
     loginForm.style.left = '50%';
     registerForm.style.left = '-50%';
@@ -16,8 +16,8 @@ btnLoginForm.addEventListener('click', () => {
 });
 
 btnRegisterForm.addEventListener('click', () => {
-    btnLoginForm.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
-    btnRegisterForm.style.backgroundColor = '#21264D';
+    btnLoginForm.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+    btnRegisterForm.style.backgroundColor = 'rgb(29 78 216)';
 
     loginForm.style.left = '150%';
     registerForm.style.left = '50%';
