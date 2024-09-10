6e41ec6acb1aaab42b538d7ef32d60ce74ee72a1
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-09-10 15:44:21 +0100

    sorted env

---
+++ create_user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Create the user and database
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+    CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';
+    CREATE DATABASE ${POSTGRES_DB};
+    GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
+EOSQL
