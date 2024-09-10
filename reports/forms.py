a9524e0959cb793fd7d133651185ff87172450b5
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-09-10 11:28:47 +0100

    generates

--- reports/forms.py
+++ reports/forms.py
@@ -70,7 +70,7 @@ class ReportForm(forms.ModelForm):
             }),
             'notes': forms.Textarea(attrs={
                 'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
-                'placeholder': 'Notes',
+                'placeholder': 'Observation Notes',
                 'rows':'2',
                 'id':'notes',
             }),
