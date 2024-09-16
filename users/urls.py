17b244ae1b6851b160c57eeff7b2b363d12a6dd8
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-09-16 10:38:48 +0100

    style

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('logout_page/', views.user_logout, name='logout_page'),
     path('login_register/', views.user_login_register, name='login_register'),
-    path('password-forgot/', views.CustomPasswordResetView.as_view(), name='password-forgot'),
+    # path('password-forgot/', views.CustomPasswordResetView.as_view(), name='password-forgot'),
  
     
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
