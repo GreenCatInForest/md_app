34c374a16a25145ab83d976b4d0ddb195c3408bd
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-10-02 22:02:23 +0100

    add report admin on page

--- reports/admin.py
+++ reports/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.db import models
 
-from core.models import Downloads
+from core.models import Downloads, Report
 
 # Register your models here.
 
@@ -10,4 +10,13 @@ class DownloadsAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'file']
     search_fields = ['name']
 
+class ReportAdmin(admin.ModelAdmin):
+    fields = ['user', 'start_time', 'end_time', 'report_timestamp', 'property_address',
+              'external_logger', 'company', 'surveyor', 'report_file']
+    list_display = ['user', 'company', 'surveyor', 'property_address', 'start_time', 'end_time', 'report_timestamp',
+              'external_logger', 'report_file']
+    search_fields = ['user', 'start_time', 'end_time', 'report_timestamp', 'property_address',
+              'external_logger', 'company', 'surveyor', 'report_file']
+
 admin.site.register(Downloads, DownloadsAdmin)
+admin.site.register(Report, ReportAdmin)
