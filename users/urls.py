10527bb701810007a981d8c48f180b4c7eb03710
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-10-17 18:02:44 +0100

    update custom views

--- users/urls.py
+++ users/urls.py
@@ -10,25 +10,18 @@ urlpatterns = [
     path('login_register/', views.user_login_register, name='login_register'),
     # path('accounts/login/', views.user_login_register, name='login_register'),
     # path('password-forgot/', views.CustomPasswordResetView.as_view(), name='password-forgot'),
-    path('password_reset_form/',
-         auth_views.PasswordResetView.as_view(
-             template_name='registration/password_reset_form.html'
-         ),
-         name='password_reset_form'),
+       # Use your custom password reset views instead of auth_views
+    path('password-forgot/',
+         views.CustomPasswordResetView.as_view(),
+         name='password-forgot'),
     path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password_reset_done.html'
-         ),
+         views.CustomPasswordResetDoneView.as_view(),
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html'
-         ),
+         views.CustomPasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
-    path('password_reset_complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password_reset_complete.html'
-         ),
+    path('password-reset-complete/',
+         views.CustomPasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
     
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
