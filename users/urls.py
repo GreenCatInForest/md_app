9f2b3881bf11ed922efa748556d1ad8acf9185c8
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-10-17 17:48:54 +0100

    try to change urls

--- users/urls.py
+++ users/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
              template_name='registration/password_reset_confirm.html'
          ),
          name='password_reset_confirm'),
-    path('password-reset-complete/',
+    path('password_reset_complete/',
          auth_views.PasswordResetCompleteView.as_view(
              template_name='registration/password_reset_complete.html'
          ),
