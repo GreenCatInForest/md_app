1559e741f52a5e3f12d0fe9552f8d48d32fc5202
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-10-30 14:49:03 +0000

    clean unnecessary

--- reports/tasks.py
+++ reports/tasks.py
@@ -20,7 +20,6 @@ from core.models import Logger as LoggerModel, Logger_Data, Room, Report
 from django.core.files import File
 
 
-
 logging.basicConfig(level=logging.DEBUG)
 app_logger = logging.getLogger(__name__)
 
@@ -99,35 +98,4 @@ def generate_report_task(self, report_id, csv_file_paths, serialized_form_data):
         app_logger.exception(f'Error generating report: {e}')
         return {'status': 'error', 'message': str(e)}
 
-# @shared_task(bind=True)
-# def generate_report_task(self, report_id):
-#     try:
-#         self.update_state(state='STARTED', meta={'status':'Initializing report generation'})
-#         # Fetch the report instance
-#         report_instance = Report.objects.select_related('user').get(id=report_id)
-#         user = report_instance.user
-#         app_logger.debug(f'Report instance saved: {report_instance}')
 
-#         self.update_state(state="PROGRESS", meta={'status':'Processing data from sensors'})
-#         time.sleep(2) 
-
-#         self.update_state(state='PROGRESS', meta={'status':'Analysing...'})
-#         time.sleep(3)
-
-#         self.update_state(state='PROPGRESS', meta={'status':'Writing report'})
-#         time.sleep(3)
-
-#         report_instance.status='Completed'
-#         report_instance.save()
-
-#         return 'Report generation completed successfully.'
-
-#     except Report.DoesNotExist:
-#         app_logger.error(f"Report with ID {report_id} does not exist.")
-#         self.update_state(state='FAILURE', meta={'status': 'Report does not exist.'})
-#         raise
-
-#     except Exception as e:
-#         app_logger.error(f"Error in report generation task: {e}")
-#         self.update_state(state='FAILURE', meta={'status': str(e)})
-#         raise e 
