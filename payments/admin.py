215796ae833e05be9126e878669761321abbfa56
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-11-26 12:22:47 +0000

    register payments in payments admin.py

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from core.models import Payment
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'amount', 'status', 'created_at', 'updated_at')
